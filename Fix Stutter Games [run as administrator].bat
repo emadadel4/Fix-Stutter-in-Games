e95c1a97b0ac27c9a848143e6d7259205e4197b1
Emad Adel <emadadel1920@gmail.com>
2023-12-27 22:28:24 +0200

    Add files via upload

---
+++ Fix Stutter Games [run as administrator].bat
@@ -0,0 +1,9 @@
+@echo off
+
+TAKEOWN /F "C:\Windows\System32\GameBarPresenceWriter.exe"
+
+RENAME "C:\Windows\System32\GameBarPresenceWriter.exe" GameBarPresenceWriter2.exe
+
+echo Telgram: https://t.me/emadadel4
+echo Watsapp: 01153377584
+pause
