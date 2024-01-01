de1536b5822ff8e0bef6c4a47d5c2ea7854d5f35
Emad Adel <emadadel1920@gmail.com>
2024-01-02 00:12:16 +0200

    Update Fix Stutter Games [run as administrator].bat

--- Fix Stutter Games [run as administrator].bat
+++ Fix Stutter Games [run as administrator].bat
@@ -1,8 +1,18 @@
 @echo off
 
-TAKEOWN /F "C:\Windows\System32\GameBarPresenceWriter.exe"
 
-RENAME "C:\Windows\System32\GameBarPresenceWriter.exe" GameBarPresenceWriter2.exe
+ICACLS "C:\Windows\System32\GameBarPresenceWriter.exe" /grant administrators:F
+
+ICACLS "C:\Windows\System32\GameBarPresenceWriter.proxy.dll.bak" /grant administrators:F
+
+ICACLS "C:\Windows\System32\Windows.Gaming.UI.GameBar" /grant administrators:F
+
+
+
+RENAME "C:\Windows\System32\GameBarPresenceWriter.exe" GameBarPresenceWriter[emadadel4].exe
+RENAME "C:\Windows\System32\GameBarPresenceWriter.proxy.dll.bak" GameBarPresenceWriter.proxy.dll[emadadel4].bak
+RENAME "C:\Windows\System32\Windows.Gaming.UI.GameBar.dll" Windows.Gaming.UI.GameBar[emadadel4].dll
+
 
 echo Telgram: https://t.me/emadadel4
 echo Watsapp: 01153377584
