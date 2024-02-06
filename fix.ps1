6fe9699a1410acfe877c9278416ce4bac68b4ec2
Emad Adel <emadadel1920@gmail.com>
2024-02-06 02:12:03 +0200

    Update and rename FixStutter.bat to fix.ps1

---
+++ fix.ps1
@@ -0,0 +1,32 @@
+takeown /f "C:\Windows\System32\GameBarPresenceWriter.exe"
+takeown /f "C:\Windows\System32\GameBarPresenceWriter.proxy.dll"
+takeown /f "C:\Windows\System32\Windows.Gaming.UI.GameBar.dll"
+
+
+timeout /T 2 /NOBREAK > null
+
+
+ICACLS "C:\Windows\System32\GameBarPresenceWriter.exe" /grant administrators:F
+ICACLS "C:\Windows\System32\GameBarPresenceWriter.proxy.dll" /grant administrators:F
+ICACLS "C:\Windows\System32\Windows.Gaming.UI.GameBar.dll" /grant administrators:F
+
+
+Rename-Item "C:\Windows\System32\GameBarPresenceWriter.proxy.dll" GameBarPresenceWriter.proxy[emadadel4].bak
+
+Rename-Item "C:\Windows\System32\GameBarPresenceWriter.exe" GameBarPresenceWriter[emadadel4].bak
+
+Rename-Item "C:\Windows\System32\Windows.Gaming.UI.GameBar.dll" Windows.Gaming.UI.GameBar[emadadel4].bak
+
+Write-Host "Renaming files,Wait..."
+
+timeout /T 3 /NOBREAK > null
+
+Write-Host "Issue fixed successfully, Enjoy Playing smooth."
+Write-Host "Vist my github to see useful Tools."
+
+Write-Host "My Website https://eprojects.orgfree.com"
+Write-Host "My Github https://github.com/emadadel4"
+Write-Host "Telgram: https://t.me/emadadel4"
+
+
+timeout /T 5 /NOBREAK > null
