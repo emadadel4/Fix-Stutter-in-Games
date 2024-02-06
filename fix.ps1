b2e987144dca683be32b167dc0ccee7fe996e6ac
Emad Adel <emadadel1920@gmail.com>
2024-02-06 02:18:43 +0200

    Update fix.ps1

--- fix.ps1
+++ fix.ps1
@@ -17,9 +17,9 @@ Rename-Item "C:\Windows\System32\GameBarPresenceWriter.exe" GameBarPresenceWrite
 
 Rename-Item "C:\Windows\System32\Windows.Gaming.UI.GameBar.dll" Windows.Gaming.UI.GameBar[emadadel4].bak
 
-Write-Host "Renaming files,Wait..."
-
 timeout /T 3 /NOBREAK > null
+
+Write-Host "Renaming files,Wait..."
 
 Write-Host "Issue fixed successfully, Enjoy Playing smooth."
 Write-Host "Vist my github to see useful Tools."
@@ -27,6 +27,3 @@ Write-Host "Vist my github to see useful Tools."
 Write-Host "My Website https://eprojects.orgfree.com"
 Write-Host "My Github https://github.com/emadadel4"
 Write-Host "Telgram: https://t.me/emadadel4"
-
-
-timeout /T 5 /NOBREAK > null
