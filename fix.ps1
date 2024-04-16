142ed5697a2f989801277b9f8def80abf93c4b26
Emad Adel <emadadel1920@gmail.com>
2024-04-16 05:59:39 +0200

    Update fix.ps1

--- fix.ps1
+++ fix.ps1
@@ -5,7 +5,8 @@ $files = @(
 )
 
 foreach ($file in $files) {
-    if (Test-Path $file) {
+    if (Test-Path $file)
+    {
         Write-Host "File $file exists."
         # Actions to perform if the file exists
         ICACLS $file /grant administrators:F
@@ -13,17 +14,23 @@ foreach ($file in $files) {
         takeown /f $file
         $newName = "$file[emadadel4].bak"
         Rename-Item $file $newName
+
+        timeout /T 2 /NOBREAK > $null
+
+        Write-Host "Issue fixed successfully. Enjoy smooth gaming experience!"
+        Write-Host "Visit my GitHub for more useful tools: https://github.com/emadadel4"
+        Write-Host "My Website: https://eprojects.orgfree.com"
+        Write-Host "Telegram: https://t.me/emadadel4"
+
     }
     else
     {
         Clear-Host
         Write-Host "The issue has already been fixed :)"
+        Write-Host "Visit my GitHub for more useful tools: https://github.com/emadadel4"
+        Write-Host "My Website: https://eprojects.orgfree.com"
+        Write-Host "Telegram: https://t.me/emadadel4"
     }
 }
 
-timeout /T 3 /NOBREAK > $null
 
-Write-Host "Issue fixed successfully. Enjoy smooth gaming experience!"
-Write-Host "Visit my GitHub for more useful tools: https://github.com/emadadel4"
-Write-Host "My Website: https://eprojects.orgfree.com"
-Write-Host "Telegram: https://t.me/emadadel4"
